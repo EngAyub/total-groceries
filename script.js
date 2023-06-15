292aed95bcfca3c913a59503fabffac83523204b
ENG AYUUB <moztaphem9@gmal.com>
2023-06-15 19:23:32 +0300

    Total groceries done

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Good Luck 💪🏾
+function getPrice(grocery){
+    let total = 0;
+
+    for(let i=0; i<grocery.length; i++){
+        total = total + grocery[i].quantity*grocery[i].price;
+    }
+
+    return total;
+}
+
+
+
+  console.log(getPrice([
+    { product: "Milk", quantity: 1, price: 1.50 },
+    { product: "Rice", quantity: 1, price: 2.50 },
+    
+  ]))
